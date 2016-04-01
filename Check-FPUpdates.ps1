8ae5060ec17540908e0eb9ab35430749bd40fded
n01d <0-d@users.noreply.github.com>
2016-04-01 16:57:27 +0300

    Parameter updates, webrequest retry added, other minor changes
    - MailTo and MailFrom type changed from [string] to [mailaddress].
    - ParameterSets added for e-mail notification usage control: both MailTo and MailFrom are mandatory if one of them used.
    - UserAgent parameter now uses [ValidateSet('InternetExplorer', 'FireFox', 'Chrome', 'Opera', 'Safari')] which sets preconfigured PowerShell useragents. No more manual useragent insertion.
    - ESR parameter now requires auth code (https://www.adobe.com/ru/products/flashplayer/distribution4.html?auth=<AUTHCODE>)/
    - If webrequest fails by timeout it will retry 10 times.
    - Some breaks replaced by exits. Otherwise script may exit FOR cycle and not the script.
    - Added version number to script description.

--- Check-FPUpdates.ps1
+++ Check-FPUpdates.ps1
@@ -2,52 +2,109 @@
 #https://www.microsoft.com/en-us/download/details.aspx?id=40855
 
 <#
-    Check for Adobe Flash Player updates
+    Download updates from Adobe to your local Flash Player update server.
     
     By n01d | https://github.com/0-d/FPUpdate
+    Version: 20160401
+
+    Syntax:
+        FPCheckUpdate.ps1 -FPIntServerRoot <string> [-FPDownloadRoot <string>] [-XMLMajor <string>] [-ESR <string>] [-Proxy <string>] [-ProxyCreds <string>] [-UserAgent <string>] [-Force] [-SmtpServer <string>] [-LogFile <string>] [<CommonParameters>]
+        FPCheckUpdate.ps1 -FPIntServerRoot <string> -MailTo <mailaddress[]> -MailFrom <mailaddress> [-FPDownloadRoot <string>] [-XMLMajor <string>] [-ESR <string>] [-Proxy <string>] [-ProxyCreds <string>] [-UserAgent <string>] [-Force] [-SmtpServer <string>] [-LogFile <string>] [<CommonParameters>]
+
+    Example:
+        .\FPCheckUpdate.ps1 -FPIntServerRoot 'fp-update.domain.local' -ESR '123098abc-blah-blah-blah-098765432' -Proxy 'http://proxy.domain.local' -UserAgent InternetExplorer -Force -MailTo 'admin@company.com','support@company.com' -MailFrom 'FP@company.com' -SmtpServer 'smtp.company.com'
 #>
 
+[CmdletBinding(DefaultParametersetName="Default")]
 param(
     #SERVER
-    [parameter(mandatory=$true)]
+    [parameter(ParameterSetName='WithMail',mandatory=$true)]
+    [parameter(ParameterSetName='Default',mandatory=$true)]
     [string]$FPIntServerRoot,
     [string]$FPDownloadRoot = 'fpdownload2.macromedia.com/pub/flashplayer/update/current/sau',
     [string]$XMLMajor = "currentmajor.xml",
-    [switch]$ESR,
+    [string]$ESR,
 
     #WEBREQUEST
-    [string]$DownloadProxy,
+    [string]$Proxy,
     [string]$ProxyCreds,
+    [ValidateSet('InternetExplorer', 'FireFox', 'Chrome', 'Opera', 'Safari')]
     [String]$UserAgent,
     [switch]$Force,
 
     #MAIL
-    [parameter(mandatory=$true)]
-    [string[]]$MailTo,
-    [parameter(mandatory=$true)]
-    [string]$MailFrom,
-    [string]$SmtpServer
+    [parameter(ParameterSetName='WithMail',mandatory=$true)]
+    [mailaddress[]]$MailTo,
+    [parameter(ParameterSetName='WithMail',mandatory=$true)]
+    [mailaddress]$MailFrom,
+    [string]$SmtpServer,
+
+    #MISC
+    [string]$LogFile = "$env:TEMP\FP.CheckUpdates.log"
 )
 
 $FPIntServerRoot += '/pub/flashplayer/update/current/sau'
+if ($ESR) {
+    $ESR = "https://www.adobe.com/ru/products/flashplayer/distribution4.html?auth=$ESR"
+    Write-Debug -Message "ESR link: [$ESR]"
+}
+
+function Write-Log {
+    param(
+        [parameter(mandatory=$true)]
+        [string]$Message,
+        [ValidateSet('Error','Warning','Info')]
+        [string]$Type = 'Info',
+        [ValidateSet('Black', 'DarkBlue', 'DarkGreen', 'DarkCyan', 'DarkRed', 'DarkMagenta', 
+        'DarkYellow', 'Gray', 'DarkGray', 'Blue', 'Green', 'Cyan', 'Red', 'Magenta', 'Yellow', 'White')]
+        [string]$Color,
+        [switch]$NoNewLine
+    )
+
+    $InLine = @{}
+    if ($NoNewLine) {
+        $InLine['NoNewLine'] = $true
+    }
+
+    if (!$Color) {
+        switch ($Type) {
+            'Error' {$Color = 'Red'; break}
+            'Warning' {$Color = 'Yellow'; break}
+            'Info' {$Color = 'Gray'; break}
+        }
+    }
+
+    if ( !(Test-Path $LogFile) ) {
+        New-Item -Path $LogFile -ItemType File -Force
+    }
+    elseif ((Get-Item $LogFile).CreationTime -lt [datetime]::Today) {
+        # Если создан не сегодня - обнуляем!
+        $null | Out-File -FilePath $LogFile -Force -Encoding utf8
+    }
+
+    Write-Host -ForegroundColor $color $Message @InLine
+    Write-Output "[$(Get-Date -Format 'dd.MM.yy HH.mm.ss')][$Type]`t$Message" | Out-File -FilePath $LogFile -Encoding utf8 -Append
+}
+
+Write-Log ">--------- SCRIPT STARTED --------->"
 
 #region WEBREQUEST PARAMS
 $WebrequestParams = @{}
 
-if ($DownloadProxy) {
-    $WebrequestParams['Proxy']=$DownloadProxy
+if ($Proxy) {
+    $WebrequestParams['Proxy']=$Proxy
 }
 
 if ($ProxyCreds) {
     $cred = Get-Credential $ProxyCreds
     $WebrequestParams['ProxyCredential']=$cred
 }
-elseif ($DownloadProxy -and !$ProxyCreds) {
+elseif ($Proxy -and !$ProxyCreds) {
     $WebrequestParams['ProxyUseDefaultCredentials']=$true
 }
 
 if ($UserAgent) {
-    $WebrequestParams['UserAgent']=$UserAgent
+    $WebrequestParams['UserAgent'] = [Microsoft.PowerShell.Commands.PSUserAgent]::$UserAgent
 }
 
 if ($Force) {
@@ -77,7 +134,7 @@ if ($SmtpServer) {
 #endregion
 
 #region GET INTERNAL SERVER VERSION
-Write-Host -f Gray "Getting INTERNAL server version..." -NoNewline
+Write-Log "Getting INTERNAL server version...`t" -NoNewline
 try{
     ## MAJOR
     $VersionMajor = $TempData = $null
@@ -92,60 +149,105 @@ try{
     $FullAX = $Full.version.ActiveX
     $FPServerVersion = "$($FullAX.major).$($FullAX.minor).$($FullAX.buildMajor).$($FullAX.buildMinor)"
 
-    Write-Host -f Green "$FPServerVersion"
+    Write-Log -Color Green "$FPServerVersion"
     Remove-Item $TempXML -Force -ErrorAction SilentlyContinue
 }
 catch {
-    Write-Host -f Red 'FAIL!'
-    Write-Host -f Red "Can't verify INTERNAL server version! Error: [$($_.Exception.Message)]."
+    Write-Log -Type Error 'FAIL!'
+    Write-Log -Type Error "Can't verify INTERNAL server version! Error: [$($_.Exception.Message)]."
+    Write-Log "<--------- SCRIPT FINISHED ---------<"
+    if ($MailFrom) {
+        Send-MailMessage -Subject "Error checking FlashPlayer version!" -Body $("Error details:`n"+$_) -Encoding UTF8 -Attachments $LogFile @MailParams
+    }
     break
 }
 #endregion
 
 #region GET ADOBE VERSION
+for ($i = 0; $i -le 10; $i++) {
+    if ($ESR) {
+        ## ESR VERSION CHECK
+        Write-Log "Getting ADOBE server version (ESR)...`t" -NoNewline
+        try{
+            $tempfile = [system.io.path]::GetTempFileName()
+            Invoke-WebRequest -Uri $ESR -OutFile $tempfile -ErrorAction Stop @WebrequestParams
+        
+            $versions = @()
+            $rx = [System.Text.RegularExpressions.Regex]('(?:[^\d]|^)(?<v>\d+\.\d+\.\d+\.\d+)(?:[^\d]|$)')
+            $m = $rx.Matches((Get-Content $tempfile | Select-String -SimpleMatch '<h1>Extended Support Release').ToString())
 
+            #Write-Host "Found [$($m.Count)] matches"
+            $m | ForEach-Object {
+                try {
+                    $local:ver = [System.Version] ($_.Groups['v'].Value)
+                    $versions +=  @($local:ver)
+                }
+                catch {
+                    Write-Host "Error [$($_.Exception.Message)]"
+                }
+            }
 
-if ($ESR) {
-    ## ESR VERSION CHECK
-    Write-Host -f Gray "Getting ADOBE server version (ESR)..." -NoNewline
-    try{
-        $tempfile = New-TemporaryFile
-        Invoke-WebRequest -Uri 'https://www.adobe.com/products/flashplayer/distribution3.html' -OutFile $tempfile @WebrequestParams
-        $FullVersion = (Get-Content $tempfile | Select-String -SimpleMatch '<h1>Extended Support Release').ToString().Split(' <')[-2]
+            $FullVersion = ($($versions | Measure-Object -Maximum ).Maximum).ToString()
+            $ESRMajorVersion = $FullVersion.Split('.')[0]
+
+            $ESRDownloadURI = (Get-Content $tempfile | Select-String -SimpleMatch "/fp_background_update_$ESRMajorVersion.cab").ToString().Split('"')[1]
     
-        Write-Host -f Green "$FullVersion"
-        Remove-Item $tempfile -Force -ErrorAction SilentlyContinue
+            Write-Log -Color Green "$FullVersion"
+            $i = 11 # Stop FOR
+            Remove-Item $tempfile -Force -ErrorAction SilentlyContinue
+        }
+        catch {
+            if ($_ -like '*The operation has timed out.*') {
+                Write-Log -Type Error 'Timeout!'
+                # Next iteration
+            }
+            else {
+                Write-Log -Type Error 'FAIL!'
+                Write-Log -Type Error "Can't verify MACROMEDIA server version (ESR)! Error: [$($_.Exception.Message)]."
+                Write-Log "<--------- SCRIPT FINISHED ---------<"
+                if ($MailFrom) {
+                    Send-MailMessage -Subject "Error checking FlashPlayer version (ESR)!" -Body $("Error details:`n"+$_) -Encoding UTF8 -Attachments $LogFile @MailParams
+                }
+                Exit
+            }
+        }
     }
-    catch {
-        Write-Host -f Red 'FAIL!'
-        Write-Host -f Red "Can't verify ADOBE server version! Error: [$($_.Exception.Message)]."
-        break
-    }
-}
-else {
-    ## PUBLIC VERSION CHECK
-    Write-Host -f Gray "Getting ADOBE server version..." -NoNewline
-    try {
-        ## MAJOR
-        $VersionMajor = $TempData = $null
-        $TempData = Invoke-WebRequest -Uri "https://$FPDownloadRoot/$XMLMajor" -ErrorAction Stop @WebrequestParams
-        $VersionMajor = ($TempData.Content).split('"')[1]
+    else {
+        ## PUBLIC VERSION CHECK
+        Write-Log "Getting ADOBE server version...`t" -NoNewline
+        try {
+            ## MAJOR
+            $VersionMajor = $TempData = $null
+            $TempData = Invoke-WebRequest -Uri "https://$FPDownloadRoot/$XMLMajor" -ErrorAction Stop @WebrequestParams
+            $VersionMajor = ($TempData.Content).split('"')[1]
 
-        ## FULL
-        $XMLFull = "https://$FPDownloadRoot/$VersionMajor/xml/version.xml"
-        $TempXML = [System.IO.Path]::GetTempFileName()
-        Invoke-WebRequest -Uri $XMLFull -OutFile $TempXML -ErrorAction Stop @WebrequestParams
-        [xml]$Full = Get-Content -Path $TempXML -ErrorAction Stop
-        $FullAX = $Full.version.ActiveX
-        $FullVersion = "$($FullAX.major).$($FullAX.minor).$($FullAX.buildMajor).$($FullAX.buildMinor)"
+            ## FULL
+            $XMLFull = "https://$FPDownloadRoot/$VersionMajor/xml/version.xml"
+            $TempXML = [System.IO.Path]::GetTempFileName()
+            Invoke-WebRequest -Uri $XMLFull -OutFile $TempXML -ErrorAction Stop @WebrequestParams
+            [xml]$Full = Get-Content -Path $TempXML -ErrorAction Stop
+            $FullAX = $Full.version.ActiveX
+            $FullVersion = "$($FullAX.major).$($FullAX.minor).$($FullAX.buildMajor).$($FullAX.buildMinor)"
 
-        Write-Host -f Green "$FullVersion"
-        Remove-Item $TempXML -Force -ErrorAction SilentlyContinue
-    }
-    catch {
-        Write-Host -f Red 'FAIL!'
-        Write-Host -f Red "Can't verify ADOBE server version! Error: [$($_.Exception.Message)]."
-        break
+            Write-Log -Color Green "$FullVersion"
+            $i = 11 # Stop FOR
+            Remove-Item $TempXML -Force -ErrorAction SilentlyContinue
+        }
+        catch {
+            if ($_ -like '*The operation has timed out.*') {
+                Write-Log -Type Error 'Timeout!'
+                # Next iteration
+            }
+            else {
+                Write-Log -Type Error 'FAIL!'
+                Write-Log -Type Error "Can't verify MACROMEDIA server version! Error: [$($_.Exception.Message)]."
+                Write-Log "<--------- SCRIPT FINISHED ---------<"
+                if ($MailFrom) {
+                    Send-MailMessage -Subject "Error checking FlashPlayer version!" -Body $("Error details:`n"+$_) -Encoding UTF8 -Attachments $LogFile @MailParams
+                }
+                Exit
+            }
+        }
     }
 }
 #endregion
@@ -157,39 +259,59 @@ else {
 if ( !([version]::TryParse($FPServerVersion,[ref]$FPServerVersionParsed)) ) {
 
     $Message = "Can't parse INTERNAL server version: [$FPServerVersion]!"
-    Write-Host -f Red $Message
-    Send-MailMessage -Body $Message -Subject 'FLASH PLAYER UPDATE ERROR' -Encoding UTF8 -BodyAsHtml @MailParams
+    Write-Log -Type Error $Message
+    if ($MailFrom) {
+        Send-MailMessage -Body $Message -Subject 'FLASH PLAYER UPDATE ERROR' -Encoding UTF8 -BodyAsHtml @MailParams
+    }
+    Write-Log "<--------- SCRIPT FINISHED ---------<"
     break
 }
 ## TRY PARSE ADOBE VERSION
 if ( !([version]::TryParse($FullVersion,[ref]$FPAdobeVersionParsed)) ) {
 
     $Message = "Can't parse ADOBE server version: [$FullVersion]!"
-    Write-Host -f Red $Message
-    Send-MailMessage -Body $Message -Subject 'FLASH PLAYER UPDATE ERROR' -Encoding UTF8 -BodyAsHtml @MailParams
+    Write-Log -Type Error $Message
+    if ($MailFrom) {
+        Send-MailMessage -Body $Message -Subject 'FLASH PLAYER UPDATE ERROR' -Encoding UTF8 -BodyAsHtml @MailParams
+    }
+    Write-Log "<--------- SCRIPT FINISHED ---------<"
     break
 }
 
 if ($FPAdobeVersionParsed -gt $FPServerVersionParsed) {
-    $Message = "Update is available!`n`tAdobe version:`t[$FullVersion]`n`tServer version:`t[$FPServerVersion]"
-    $HTMLMessage = "<b>Update is available!</b><br /><br />
-    Adobe version: <b>$FullVersion</b><br />
-    Server version: <b>$FPServerVersion</b><br /><br />
-    <a href='https://www.adobe.com/products/flashplayer/distribution3.html'>Download link</a>"
+
+        if ($ESRDownloadURI) {
+            $DownloadLink = $ESRDownloadURI
+        }
+        else {
+            $DownloadLink = 'https://www.adobe.com/products/flashplayer/distribution3.html'
+        }
+
+        $Message = "Update is available!`n`tAdobe version:`t[$FullVersion]`n`tServer version:`t[$FPServerVersion]"
+        $HTMLMessage = "<b>Update is available!</b><br /><br />
+        Adobe version: <b>$FullVersion</b><br />
+        Server version: <b>$FPServerVersion</b><br /><br />
+        <a href='$DownloadLink'>Download</a><br />
+        Link: $DownloadLink"
     
-    Write-Host -f Gray "Sending mail... "
-    try {
-        Send-MailMessage -Body $HTMLMessage -Subject 'FLASH PLAYER UPDATE AVAILABLE' -Encoding UTF8 -BodyAsHtml @MailParams
-        Write-Host -f Green 'OK'
-    }
-    catch {
-        Write-Host -f Red 'FAIL'
-        Write-Host -f Red "Mail not sent! Error was: [$($_.Exception.Message)]."
-        break
-    }
+        Write-Log -Color Green $Message
+        Write-Log "Sending mail... " -NoNewline
+        try {
+            if ($MailFrom) {
+                Send-MailMessage -Body $HTMLMessage -Subject 'FLASH PLAYER UPDATE AVAILABLE' -Encoding UTF8 -BodyAsHtml -ErrorAction Stop @MailParams
+            }
+            Write-Log -Color Green 'OK'
+        }
+        catch {
+            Write-Log -Type Error 'FAIL'
+            Write-Log -Type Error "Mail not sent! Error was: [$($_.Exception.Message)]."
+            Write-Log "<--------- SCRIPT FINISHED ---------<"
+            break
+        }
 }
 else {
-    Write-Host -f Gray "Flash Player is Up-To-Date."
+    Write-Log "Flash Player is Up-To-Date."
 }
-
 #endregion
+
+Write-Log "<--------- SCRIPT FINISHED ---------<"
